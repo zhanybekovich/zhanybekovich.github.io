85ca382cf69173bf2e5424995624de9587973ac3
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-04-26 11:58:12 +0600

    Add mobile menu

--- html-css/neuro-trainers/js/main.js
+++ html-css/neuro-trainers/js/main.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+const menuOpen = document.querySelector("#menu-open");
+const mobileMenu = document.querySelector(".mobile-menu-wrapper");
+const menuClose = document.querySelector("#menu-close");
+
+function toggleMenu() {
+  mobileMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+  mobileMenu.classList.remove("active");
+}
+
+menuOpen.addEventListener("click", toggleMenu);
+menuClose.addEventListener("click", closeMenu);
